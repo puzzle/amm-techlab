db4be315f3a76c57e40b8af28346b214c98dd92d
Philip Stark <stark@puzzle.ch>
2020-10-22 09:01:31 +0200

    adjust start hugo script

--- start_hugo.sh
+++ start_hugo.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --rm -v $(pwd):/opt/app/src -w /opt/app/src -p 8081:8080 acend/hugo:0.70.0 hugo server -p 8080 --bind 0.0.0.0
+docker run -it --rm -v $(pwd):/opt/app/src -w /opt/app/src -p 8081:8081 acend/hugo:0.70.0 hugo server -p 8081 --bind 0.0.0.0
